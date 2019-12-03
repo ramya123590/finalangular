17efc8e9f94fe9ef80c664f6b3f35af848fb7777
kokkarneakshatha <kokkarneakshatha@gmail.com>
2019-12-03 09:57:32 +0530

    first commit

---
+++ src/app/model/appointment.ts
@@ -0,0 +1,15 @@
+import { Time } from '@angular/common';
+
+export class Appointment {
+       id:Number;
+	
+	
+	   date:Date;
+
+	   slot:Time;
+	firstName:String;
+	
+	doctor_id:Number;
+	patient_id:String;
+	  isfeepaid: boolean;
+}
---
+++ src/app/service/apoointment.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { Doctor } from '../model/doctor';
+import { Branch } from '../model/branch';
+import { Specialist } from '../model/specialist';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Appointment } from '../model/appointment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApoointmentService {
+
+ 
+  private usersUrl: string;
+ 
+  constructor(private http: HttpClient) {
+    this.usersUrl = 'http://localhost:1234/api/appointment';
+  }
+  public findAll(): Observable< Appointment[]> {
+    return this.http.get< Appointment[]>(this.usersUrl);
+  }
+  public find(app:Appointment): Observable<any> {
+    return this.http.post< Appointment>("http://localhost:1234/api/find",app);
+  }
+  public Appoinmentdetails(app:Appointment): Observable<Appointment[]> {
+    return this.http.post< Appointment[]>("http://localhost:1234/api/appointment_details",app);
+  }
+
+  public save(user: Appointment) {
+    return this.http.post<Appointment>(this.usersUrl, user);
+  }
+  deleteAppointment(id: Number): Observable<any> {
+    return this.http.delete(`${this.usersUrl}/${id}`, { responseType: 'text' });
+  }
+  
+  
+}
